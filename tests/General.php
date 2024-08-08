dbd10652288438d674538392c9163e94eb590144
CI <ci@b-teka.com>
2024-08-08 15:44:14 +0700

    Fix

--- tests/General.php
+++ tests/General.php
@@ -10,16 +10,18 @@ final class General extends TestCase
         return new Client($api = '9HAKsYHqWyYjil9Yrfq3pBKKSmYzmgeGhgpur3Dg4CYomgWkXy2YqzyQITbi');
     }
 
-    public function testVerifyEndpointWorks(): void
+    public function testVerifyASingleEmailAddress(): void
     {
         $email = 'zyx@gmail.com';
 
         $response = $this->getClient()->verify($email);
 
-        $this->assertTrue(isset($response['email']));
+        // return [ 'status' => 'deliverable' ]
+
+        $this->assertTrue(  array_keys_exists('status', $response) );
     }
 
-    public function testBatchVerifyEndpointWorks(): void
+    public function testSendBatchOfEmailsForVerification(): void
     {
         $emails = [
             'zyx@yahoo.com',
@@ -28,6 +30,22 @@ final class General extends TestCase
 
         $response = $this->getClient()->batchVerify($emails);
 
-        $this->assertTrue(isset($response['results']));
+        $this->assertTrue( array_keys_exists( 'batch_id', $response ) );
+    }
+
+    public function testGetBatchStatus(): void
+    {
+        $batchId = '3982472938473928';
+        $response = $this->getClient()->getBatchStatus($batchId);
+
+        $this->assertTrue( $response['status'] = 'running' );
+    }
+
+    public function testGetBatchResult()
+    {
+        $batchId = '3982472938473928';
+        $response = $this->getClient()->getBatchResult($batchId);
+
+        $this->assertTrue( array_keys_exists('emails', $response) );
     }
 }
